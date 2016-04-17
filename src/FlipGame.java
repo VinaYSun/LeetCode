f4a8f36650c4b7858218f8b3d591827b531582d5
yaweisun <sunyaweiwei@gmail.com>
2016-04-17 16:50:46 -0400

    refact

--- src/FlipGame.java
+++ src/FlipGame.java
@@ -1,5 +1,3 @@
-import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,26 +5,26 @@ import java.util.List;
  * Created by ysun on 3/26/16.
  */
 public class FlipGame {
-    @Test
-    public void test() {
-        String[] testcase = {"+", "-", "++", "--", "+++", "++++", "+--+", "-+-+", "+-+-"};
-        for (int i = 0; i < testcase.length; i++) {
-            System.out.println(getFlipResult(testcase[i]));
-        }
+
+    public static void main(String[] args) {
+        FlipGame fg = new FlipGame();
+        String s = "+---++--+++-+-+--+";
+        List<String> result = fg.getFlipResult(s);
+        System.out.println(result);
     }
 
-    public List<String> getFlipResult(String s1) {
+    public List<String> getFlipResult(String s) {
         List<String> res = new ArrayList<String>();
-        if (s1.length() < 2) return res;
-        search(s1, res, 0);
+        if (s.length() < 2) return res;
+        search(s, res, 0);
         return res;
     }
 
-    public void search(String s1, List<String> res, int len) {
-        for (int i = len; i < s1.length() - 1; i++) {
-            if (s1.charAt(i) == '+' && s1.charAt(i) == s1.charAt(i + 1)) {
-                String t1 = s1.substring(0, i) + "--" + s1.substring(i + 2);
-                res.add(t1);
+    public void search(String s, List<String> res, int len) {
+        for (int i = len; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '+' && s.charAt(i) == s.charAt(i + 1)) {
+                String t = s.substring(0, i) + "--" + s.substring(i + 2);
+                res.add(t);
             }
         }
     }
