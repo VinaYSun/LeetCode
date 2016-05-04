d50ad6c099bc49154a15f37de616fc95e59bd5c5
yaweisun <sunyaweiwei@gmail.com>
2016-05-04 19:09:04 -0400

    format

--- src/main/ClassicBinarySearch.java
+++ src/main/ClassicBinarySearch.java
@@ -7,14 +7,13 @@ package main;
 public class ClassicBinarySearch {
 
     public int BinarySearch(int[] nums, int target) {
-        if ( nums == null || nums.length == 0 ){
+        if (nums == null || nums.length == 0){
             return -1;
         }
         int start, end, mid;
         start = 0;
         end = nums.length - 1;
-
-        while ( start + 1 < end ) {
+        while (start + 1 < end) {
             mid = start + (end - start) / 2; // re-calculate mid in every loop
             if (nums[mid] == target) {
                 end = mid;
