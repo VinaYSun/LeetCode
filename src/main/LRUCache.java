df530323c5948cfc7d77a148dce2e81743e2a816
yaweisun <sunyaweiwei@gmail.com>
2016-05-10 22:05:33 -0400

    Passed test

--- src/main/LRUCache.java
+++ src/main/LRUCache.java
@@ -6,12 +6,13 @@ import java.util.HashMap;
  * Double LinkedList : remove/insert : O(1)
  * HashMap : search : O(1)
  *
- * Head : Most Recently Used
- * Tail : Least Recently Used
+ * Head.next -- least recently visit
+ * Tail.prev -- recently visit
  *
  * 每次访问linkedlist上的点，必然进行list的序列更新：
  * —— remove被访问的点，将其重置为head
  *
+ * 参考自 九章算法
  *
  */
 public class LRUCache {
@@ -29,82 +30,52 @@ public class LRUCache {
     }
 
     private int capacity;
-    private int length;
     private HashMap<Integer, Node> map;
-    private Node head;
-    private Node tail;
+    private Node head = new Node(-1, -1);
+    private Node tail = new Node(-1, -1);
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        head = null;
-        tail = null;
-        length = 0;
-        map = new HashMap<Integer, Node>();
+        head.next = tail;
+        tail.prev = head;
+        this.map = new HashMap<Integer, Node>();
     }
 
     public int get(int key) {
         if (!map.containsKey(key)) {
             return -1;
         } else {
-            remove(map.get(key));
-            setHead(map.get(key));
+            Node current = map.get(key);
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+
+            saveToTail(current);
             return map.get(key).val;
         }
     }
 
     public void set(int key, int value) {
-        if (!map.containsKey(key)) {
-            Node current = new Node(key, value);
-            if (length < capacity) {
-                setHead(current);
-                map.put(key, current);
-                length++;
-            } else {
-                map.remove(tail);
-                // don't forget to set up new tail
-                tail = tail.prev;
-                if (tail != null) {
-                    tail.next = null;
-                }
-
-                setHead(current);
-                map.put(key, current);
-            }
-        } else {
-            Node oldNode = map.get(key);
-            Node newNode = new Node(key, value);
-            remove(oldNode);
-            map.put(key, newNode);
-            setHead(newNode);
+        if (get(key) != -1) {
+            map.get(key).val = value;
+            return;
         }
+
+        if (map.size() == capacity) {
+            map.remove(head.next.key);
+            head.next = head.next.next;
+            head.next.prev = head;
+        }
+
+        Node insert = new Node(key, value);
+        map.put(key, insert);
+        saveToTail(insert);
     }
 
-    public void remove(Node node) {
-        if (node.prev != null) {
-            node.prev.next = node.next;
-        } else {
-            head = node.next;
-        }
-
-        if (node.next != null) {
-            // @.@
-            node.next.prev = node.prev;
-        } else {
-            tail = node.prev;
-        }
+    public void saveToTail(Node node) {
+        node.prev = tail.prev;
+        tail.prev = node;
+        node.prev.next = node;
+        node.next = tail;
     }
 
-    public void setHead(Node node) {
-        node.next = head;
-        node.prev = null;
-
-        if (head != null) {
-            head.prev = node;
-        }
-
-        head = node;
-        if (tail == null) {
-            tail = head;
-        }
-    }
 }
