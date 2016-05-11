9ea6918b004a34db9462c55e431babdff5636171
yaweisun <sunyaweiwei@gmail.com>
2016-05-10 23:47:17 -0400

    Summary Ranges

--- src/main/SummaryRanges.java
+++ src/main/SummaryRanges.java
@@ -5,47 +5,24 @@ import java.util.List;
 
 /**
  *
- * 几处边界条件：
- * 1. 长度为零，返回空
- * 2. 长度为1，返回唯一值
- * 3. 循环从第二位开始
- *
  */
 public class SummaryRanges {
 
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<String>();
-
         if (nums == null || nums.length == 0) {
             return result;
         }
-
-        if (nums.length == 1) {
-            result.add(String.valueOf(nums[0]));
-        }
-
-        int start = nums[0];
-        int end = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == end + 1) {
-                if (i == nums.length - 1) {
-                    result.add(start + "->" + nums[i]);
-                }
-            } else {
-                if (start == end) {
-                    result.add(start + "");
-                } else {
-                    result.add(start + "->" + end);
-                }
-
-                if (i == nums.length - 1) {
-                    result.add(nums[i] + "");
-                }
-
-                start = nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            while ( i < nums.length - 1 && nums[i] == nums[i+1] - 1){
+                i++;
             }
-            end = nums[i];
+            if (nums[i] == start){
+                result.add(String.valueOf(start));
+            } else {
+                result.add(start + "->" + nums[i]);
+            }
         }
         return result;
     }
