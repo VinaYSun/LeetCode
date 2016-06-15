87316ca50d9d579f217d67f6762f22c990ddfb57
yaweisun <sunyaweiwei@gmail.com>
2016-06-14 21:34:02 -0400

    6.14

--- src/may/UniquePaths.java
+++ src/may/UniquePaths.java
@@ -8,18 +8,22 @@ public class UniquePaths {
         if (m == 0 || n == 0) {
             return 0;
         }
-        int[][] sum = new int[m][n];
+        // m * n size
+        int[][] f = new int[m][n];
         for (int i = 0; i < m; i++) {
-            sum[i][0] = 1;
+            f[i][0] = 1;
         }
         for (int i = 0; i < n; i++) {
-            sum[0][i] = 1;
+            f[0][i] = 1;
         }
+        // start from [1, 1] -- [second row, second column]
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                sum[i][j] = sum[i - 1][j] + sum[i][j - 1];
+                // the question is 'how many paths'
+                // so solution is a sum from two directions
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
             }
         }
-        return sum[m - 1][n - 1];
+        return f[m - 1][n - 1];
     }
 }
